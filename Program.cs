e47061d3c1bc8d51272bcad910219c31064dbe11
Eduardo Garcia <eduardogarcia2001@gmail.com>
2023-10-02 16:27:51 -0400

    Challenge Done

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Nokia");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "3350", imei: "77777", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Fotos");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Iphone");
+Smartphone iphone = new Iphone(numero: "987654", modelo: "15", imei: "999999", memoria: 512);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Banco");
